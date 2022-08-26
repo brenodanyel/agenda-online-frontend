10360003480e2baf49df3131240c087650d34f6f
brenodanyel <bdls1704@hotmail.com>
2022-08-25 23:56:30 -0300

    create home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { AuthContextProvider } from './contexts/auth.context';
+import { Home } from './pages/home';
 import { Login } from './pages/login';
 import { Register } from './pages/register';
 import { Notfound } from './pages/not_found';
@@ -10,7 +11,7 @@ export function App() {
     <BrowserRouter>
       <AuthContextProvider>
         <Routes>
-          <Route path='/' element={<Navigate to="/login" />} />
+          <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path='*' element={<Notfound />} />
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+import style from './home.module.scss';
+
+export function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
