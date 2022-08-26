46d7367f5b73d90355746fe919d5f0903b357937
brenodanyel <bdls1704@hotmail.com>
2022-08-25 22:06:57 -0300

    feat: add button variants

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,19 +2,21 @@ import style from './button.module.scss';
 
 type ButtonProps = {
   text: string;
-  onClick(): void;
+  onClick?(): void;
+  variant?: 'default' | 'outlined';
 };
 
 export function Button(props: ButtonProps) {
   const {
     text,
     onClick,
+    variant = 'default',
   } = props;
 
   return (
     <button
       onClick={onClick}
-      className={style.button}
+      className={`${style.button} ${style[variant]}`}
     >
       {text}
     </button>
