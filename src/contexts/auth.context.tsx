27a2fd190c14886533c8bdba82d1c39a29f53f80
brenodanyel <bdls1704@hotmail.com>
2022-08-26 00:00:07 -0300

    update auth.context: add user, token and verifyToken

--- src/contexts/auth.context.tsx
+++ src/contexts/auth.context.tsx
@@ -1,4 +1,6 @@
-import { createContext, ReactNode } from 'react';
+import { createContext, useEffect, useState, ReactNode } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
 
 type signInParams = {
   username: string;
@@ -11,13 +13,23 @@ type signUpParams = {
   password: string;
 };
 
+type user = {
+  id: string;
+  username: string;
+  email: string;
+};
+
 type AuthContextType = {
+  user: user | null;
+  token: string | null;
   signIn(params: signInParams): Promise<void>;
   signUp(params: signUpParams): Promise<void>;
   signOut(): void;
 };
 
 const defaultValue: AuthContextType = {
+  user: null,
+  token: localStorage.getItem('token'),
   signIn: async () => { },
   signUp: async () => { },
   signOut: () => { },
@@ -31,20 +43,55 @@ type AuthContextProviderProps = {
 
 export const AuthContextProvider = (props: AuthContextProviderProps) => {
   const { children } = props;
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState(defaultValue.user);
+  const [token, setToken] = useState(defaultValue.token);
 
   const signIn = async (params: signInParams) => {
     console.log(params);
+    toast.success('Login efetuado com sucesso');
   };
 
   const signUp = async (params: signUpParams) => {
     console.log(params);
+    toast.success('Conta criada com sucesso');
   };
 
   const signOut = () => {
-
+    setUser(null);
+    setToken(null);
   };
 
+  useEffect(() => {
+    if (['/login', '/register'].includes(pathname)) return;
+
+    const verifyToken = async () => {
+      try {
+        if (!token) throw new Error('token is missing');
+        // todo: make a request to back-end, verify token and replace the user prop
+      } catch (e: any) {
+        console.log(e.message);
+        setUser(null);
+        setToken(null);
+        navigate('/login');
+      }
+    };
+
+    verifyToken();
+  }, [location, token]);
+
+  // sync token prop with localStorage
+  useEffect(() => {
+    token
+      ? localStorage.setItem('token', token)
+      : localStorage.removeItem('token');
+  }, [token]);
+
   const value = {
+    user,
+    token,
     signIn,
     signUp,
     signOut,
