a7b2627f82d7e7e6ef406755cf886042afed1166
brenodanyel <bdls1704@hotmail.com>
2022-08-29 16:37:48 -0300

    add icon to signout button

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,4 @@
+import { RiLogoutBoxRLine } from 'react-icons/ri';
 import { useAuth } from '../../hooks/useAuth';
 import { Button } from '../button';
 
@@ -13,7 +14,7 @@ export function Header() {
       </div>
       <div className={style.rightSide}>
         <span>{user?.username}</span>
-        <Button text="Sair" onClick={signOut} />
+        <Button text="Sair" onClick={signOut} icon={<RiLogoutBoxRLine size={18} />} />
       </div>
     </header>
   );
