28c52ed4eea2fbaec886492cc9fbde8ce8505ab5
brenodanyel <bdls1704@hotmail.com>
2022-08-29 23:18:34 -0300

    add react-modal

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Modal from 'react-modal';
 import { App } from './App';
 import './global.scss';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const root = document.getElementById('root') as HTMLElement;
+
+Modal.setAppElement(root);
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
