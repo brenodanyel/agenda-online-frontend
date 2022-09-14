eac0377769d15bec7b11ab26ab8e7881615d75f7
brenodanyel <bdls1704@hotmail.com>
2022-09-13 22:28:57 -0300

    refactor payments context and usePayments hook

--- src/contexts/payments.context.tsx
+++ src/contexts/payments.context.tsx
@@ -1,24 +1,14 @@
-import { createContext, useEffect, useState, ReactNode } from 'react';
-import { toast } from 'react-hot-toast';
-import { Range } from 'react-date-range';
-import * as paymentsApi from '../services/payments.service';
-import { useAuth } from '../hooks/useAuth';
+import { createContext, useState, ReactNode } from 'react';
 import { Payment } from '../types/payment';
 
 type PaymentsContextType = {
   payments: Payment[],
-  fetchAllPayments(range?: Range): Promise<void>;
-  addPayment(customer: string, date: Date, installments: number, price: number): Promise<void>;
-  editPayment(id: string, customer: string, date: Date, installments: number, price: number): Promise<void>;
-  removePayment(id: string): Promise<void>;
+  setPayments: React.Dispatch<React.SetStateAction<Payment[]>>;
 };
 
 const defaultValue: PaymentsContextType = {
   payments: [],
-  fetchAllPayments: async () => { },
-  addPayment: async () => { },
-  editPayment: async () => { },
-  removePayment: async () => { },
+  setPayments() { },
 };
 
 export const PaymentsContext = createContext(defaultValue);
@@ -29,69 +19,11 @@ type PaymentsContextProviderProps = {
 
 export const PaymentsContextProvider = (props: PaymentsContextProviderProps) => {
   const { children } = props;
-  const { token } = useAuth();
 
   const [payments, setPayments] = useState(defaultValue.payments);
 
-  const fetchAllPayments = async (range?: Range) => {
-    if (!token) return;
-    try {
-      const payments = await paymentsApi.findByUser(token, range);
-      setPayments(payments);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  useEffect(() => {
-    fetchAllPayments();
-  }, []);
-
-  const removePayment = async (id: string) => {
-    if (!token) return;
-    try {
-      await paymentsApi.deleteByUser(token, id);
-      setPayments((state) => {
-        return state.filter((payment) => payment.id !== id);
-      });
-      toast.success('Pagamento removido com sucesso!');
-    } catch (e: any) {
-      toast.error(e?.response?.data?.error ?? 'Erro desconhecido');
-    }
-  };
-
-  const addPayment = async (customer: string, date: Date, installments: number, price: number) => {
-    if (!token) return;
-    try {
-      const payment = await paymentsApi.createByUser(token, customer, date, installments, price);
-      setPayments((state) => {
-        return [...state, payment];
-      });
-      toast.success('Pagamento adicionado com sucesso!');
-    } catch (e: any) {
-      toast.error(e?.response?.data?.error ?? 'Erro desconhecido');
-    }
-  };
-
-  const editPayment = async (id: string, customer: string, date: Date, installments: number, price: number) => {
-    if (!token) return;
-    try {
-      const payment = await paymentsApi.editByUser(token, id, customer, date, installments, price);
-      setPayments((state) => {
-        return state.map((_payment) => _payment.id == id ? payment : _payment);
-      });
-      toast.success('Pagamento editado com sucesso!');
-    } catch (e: any) {
-      toast.error(e?.response?.data?.error ?? 'Erro desconhecido');
-    }
-  };
-
   const value = {
-    payments,
-    fetchAllPayments,
-    addPayment,
-    editPayment,
-    removePayment,
+    payments, setPayments,
   };
 
   return (
--- src/hooks/usePayments.ts
+++ src/hooks/usePayments.ts
@@ -1,4 +1,73 @@
-import { useContext } from 'react';
-import { PaymentsContext } from '../contexts/payments.context';
+import { useContext, useEffect } from 'react';
+import { toast } from 'react-hot-toast';
+import { Range } from 'react-date-range';
 
-export const usePayments = () => useContext(PaymentsContext);
+import { PaymentsContext } from '../contexts/payments.context';
+import { useAuth } from '../hooks/useAuth';
+import * as paymentsApi from '../services/payments.service';
+
+export const usePayments = () => {
+  const { token } = useAuth();
+  const { payments, setPayments } = useContext(PaymentsContext);
+
+  const fetchAllPayments = async (range?: Range) => {
+    if (!token) return;
+    try {
+      const payments = await paymentsApi.findByUser(token, range);
+      setPayments(payments);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const removePayment = async (id: string) => {
+    if (!token) return;
+    try {
+      await paymentsApi.deleteByUser(token, id);
+      setPayments((state) => {
+        return state.filter((payment) => payment.id !== id);
+      });
+      toast.success('Pagamento removido com sucesso!');
+    } catch (e: any) {
+      toast.error(e?.response?.data?.error ?? 'Erro desconhecido');
+    }
+  };
+
+  const addPayment = async (customer: string, date: Date, installments: number, price: number) => {
+    if (!token) return;
+    try {
+      const payment = await paymentsApi.createByUser(token, customer, date, installments, price);
+      setPayments((state) => {
+        return [...state, payment];
+      });
+      toast.success('Pagamento adicionado com sucesso!');
+    } catch (e: any) {
+      toast.error(e?.response?.data?.error ?? 'Erro desconhecido');
+    }
+  };
+
+  const editPayment = async (id: string, customer: string, date: Date, installments: number, price: number) => {
+    if (!token) return;
+    try {
+      const payment = await paymentsApi.editByUser(token, id, customer, date, installments, price);
+      setPayments((state) => {
+        return state.map((_payment) => _payment.id == id ? payment : _payment);
+      });
+      toast.success('Pagamento editado com sucesso!');
+    } catch (e: any) {
+      toast.error(e?.response?.data?.error ?? 'Erro desconhecido');
+    }
+  };
+
+  useEffect(() => {
+    fetchAllPayments();
+  }, []);
+
+  return {
+    payments,
+    fetchAllPayments,
+    removePayment,
+    addPayment,
+    editPayment,
+  };
+};
